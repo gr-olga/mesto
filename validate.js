0307123ede806932275a25c4926b9731dacd217a
Olga Panfilova <gr-olga@ya.ru>
2021-07-12 13:27:12 +0200

    implement button disable

--- validate.js
+++ validate.js
@@ -1,29 +1,56 @@
+const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+
+// const buttonElement = formElement.querySelector('#popup__save');
+
+function toggleButtonState(inputList, buttonElement) {
+    if (hasInputInvalid(inputList)) {
+        buttonElement.classList.add('popup__save_invalid');
+        buttonElement.setAttribute('disable', true)
+    } else {
+        buttonElement.classList.remove('popup__save_invalid');
+        buttonElement.removeAttribute('disable')
+    }
+}
+
+function hasInputInvalid(inputList) {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid
+    });
+}
+
 function showInputError(formElement, inputElement, errorMessage) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add('popup__input_type_error');
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    console.log(inputElement);
+    inputElement.classList.add('popup__input_invalid');
     errorElement.textContent = errorMessage;
-    errorElement.classList.add('popup__input-error_active');
+    errorElement.classList.add('popup__input_type_error_active');
 }
 
 function hideInputError(formElement, inputElement) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove('popup__input_type_error');
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_invalid');
     errorElement.classList.remove('popup__input-error_active');
     errorElement.textContent = '';
 }
 
+
 function isValid(formElement, inputElement) {
-    if (inputElement.validity.valid) {
-        hideInputError(formElement, inputElement)
+    const errorMessage = inputElement.validationMessage
+
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, errorMessage);
     } else {
-        showInputError(formElement, inputElement, inputElement.validationMessage)
+        hideInputError(formElement, inputElement)
     }
+
+    const buttonElement = formElement.querySelector('.popup__save');
+    toggleButtonState(inputList, buttonElement)
 }
 
 function setEventListener(formElement) {
     const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-    inputList.forEach((inputElement) => {
-        inputElement.addEventListener('input', () => {
+    inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', (event) => {
             isValid(formElement, inputElement)
         })
     })
